a98d20718df99c015f83fbc6ee8fbe668e5c46a5
Cordet Gula <cgula@pdx.edu>
2024-04-17 20:50:41 -0700

    added title

--- chomp/src/main.rs
+++ chomp/src/main.rs
@@ -1,6 +1,24 @@
-//use std::collections::HashSet;
-//use std::env;
+#[allow(unused_imports)]
+use std::collections::HashSet;
+use std::env;
+
+const TITLE: &str = "
++===========+
+|           |
+|+-+-+-+-+-+|
+||C|h|o|m|p||
+|+-+-+-+-+-+|
+|           |
++===========+
+";
 
 fn main() {
-    println!("Hello, world!");
+    clearscreen::clear().expect("failed to clear screen");
+    println!("{}", TITLE);
+
+    let args: Vec<String> = env::args().collect();
+
+    let (_m, _n) = (&args[1], &args[2]);
+
+    // if &args[1] == "chomp" then index equals (&args[2], &args[3])
 }
